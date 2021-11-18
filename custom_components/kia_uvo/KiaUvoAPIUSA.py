52a401f0cdbce3c45e3c0593e8074b191acd1a2c
Bren Dahl <dahl.brendan@gmail.com>
2021-11-17 19:30:38 -0500

    feat: add body to request debug logging

--- custom_components/kia_uvo/KiaUvoAPIUSA.py
+++ custom_components/kia_uvo/KiaUvoAPIUSA.py
@@ -55,7 +55,11 @@ def request_with_active_session(func):
 def request_with_logging(func):
     def request_with_logging_wrapper(*args, **kwargs):
         url = kwargs["url"]
-        _LOGGER.debug(f"sending {url} request")
+        body = kwargs.get("body")
+        if body is not None:
+            _LOGGER.debug(f"sending {url} request {body}")
+        else:
+            _LOGGER.debug(f"sending {url} request")
         response = func(*args, **kwargs)
         _LOGGER.debug(f"got response {response.text}")
         response_json = response.json()
