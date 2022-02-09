fc843584ca90fb61543bad844705804246a398c4
nprez83 <nprez83@users.noreply.github.com>
2022-02-09 20:19:36 +0000

    fix: black action

--- custom_components/kia_uvo/Vehicle.py
+++ custom_components/kia_uvo/Vehicle.py
@@ -182,10 +182,10 @@ class Vehicle:
     async def start_climate(self, set_temp, duration, defrost, climate, heating):
         if set_temp is None:
             set_temp = 72
-        elif set_temp < 62: 
-            set_temp = 'LOW'
+        elif set_temp < 62:
+            set_temp = "LOW"
         elif set_temp > 82:
-            set_temp = 'HIGH'
+            set_temp = "HIGH"
         if duration is None:
             duration = 5
         if defrost is None:
