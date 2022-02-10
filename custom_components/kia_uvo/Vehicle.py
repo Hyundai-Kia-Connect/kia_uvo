bedd743061f3a462a188514ac4772db5726d6744
nprez83 <41305393+nprez83@users.noreply.github.com>
2022-02-09 21:31:15 -0500

    Update Vehicle.py

--- custom_components/kia_uvo/Vehicle.py
+++ custom_components/kia_uvo/Vehicle.py
@@ -182,11 +182,6 @@ class Vehicle:
     async def start_climate(self, set_temp, duration, defrost, climate, heating):
         if set_temp is None:
             set_temp = 21
-        if REGIONS[self.region] == REGION_USA and BRANDS[self.brand] == BRAND_KIA:
-            if set_temp < 62:
-                set_temp = "LOW"
-            elif set_temp > 82:
-                set_temp = "HIGH"
         if duration is None:
             duration = 5
         if defrost is None:
