491d37d95ee4f67a731f75565e496d71384c80a5
cdnninja <jaydenaphillips@gmail.com>
2022-02-10 11:13:24 -0700

    Services

--- custom_components/kia_uvo/services.py
+++ custom_components/kia_uvo/services.py
@@ -12,8 +12,14 @@ from .const import DOMAIN
 
 SERVICE_UPDATE = "update"
 SERVICE_FORCE_UPDATE = "force_update"
+SERVICE_LOCK = "lock"
+SERVICE_UNLOCK = "unlock"
+SERVICE_STOP_CLIMATE = "stop_climate"
+SERVICE_START_CLIMATE = "start_climate"
+SERVICE_START_CHARGE = "start_charge"
+SERVICE_STOP_CHARGE = "stop_charge"
 
-SUPPORTED_SERVICES = (SERVICE_UPDATE, SERVICE_FORCE_UPDATE)
+SUPPORTED_SERVICES = (SERVICE_UPDATE, SERVICE_FORCE_UPDATE, SERVICE_LOCK, SERVICE_UNLOCK, SERVICE_STOP_CLIMATE, SERVICE_START_CLIMATE, SERVICE_START_CHARGE, SERVICE_STOP_CHARGE)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,13 +33,43 @@ def async_setup_services(hass: HomeAssistant) -> None:
     
 
     async def async_handle_update(call):
-        coordinator = hass.data[DOMAIN][config_entry.unique_id]
+        coordinator = _get_coordinator_from_device(hass, call)
         await coordinator.async_update_all()
+
+    async def async_handle_start_climate(call):
+        coordinator = _get_coordinator_from_device(hass, call)
+        #await coordinator.async_update_all()
+
+    async def async_handle_stop_climate(call):
+        coordinator = _get_coordinator_from_device(hass, call)
+        #await coordinator.async_update_all()
+
+    async def async_handle_lock(call):
+        coordinator = _get_coordinator_from_device(hass, call)
+        await coordinator.async_lock_vehicle(call.data[ATTR_DEVICE_ID][0])
+
+    async def async_handle_unlock(call):
+        coordinator = _get_coordinator_from_device(hass, call)
+        await coordinator.async_unlock_vehicle(call.data[ATTR_DEVICE_ID][0])
+
+    async def async_handle_start_charge(call):
+        coordinator = _get_coordinator_from_device(hass, call)
+        #await coordinator.async_update_all()
+
+    async def async_handle_stop_charge(call):
+        coordinator = _get_coordinator_from_device(hass, call)
+        #await coordinator.async_update_all()
         
 
     services = {
         SERVICE_FORCE_UPDATE: async_handle_force_update,
         SERVICE_UPDATE: async_handle_update,
+        SERVICE_START_CLIMATE: async_handle_start_climate,
+        SERVICE_STOP_CLIMATE: async_handle_stop_climate,
+        SERVICE_LOCK: async_handle_lock,
+        SERVICE_UNLOCK: async_handle_unlock,
+        SERVICE_START_CHARGE: async_handle_start_charge,
+        SERVICE_STOP_CHARGE: async_handle_stop_charge,
     }
     
     for service in SUPPORTED_SERVICES:
