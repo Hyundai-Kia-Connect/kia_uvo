d4d5d7aee219f09bc2cdee67e9c6b2b8a5c6689b
cdnninja <jaydenaphillips@gmail.com>
2022-12-03 20:40:39 -0700

    Update coordinator.py

--- custom_components/kia_uvo/coordinator.py
+++ custom_components/kia_uvo/coordinator.py
@@ -21,7 +21,7 @@ from homeassistant.const import (
     CONF_USERNAME,
 )
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from homeassistant.util import dt as dt_util
 
 from .const import (
@@ -120,11 +120,15 @@ class HyundaiKiaConnectDataUpdateCoordinator(DataUpdateCoordinator):
                     self.vehicle_manager.check_and_force_update_vehicles,
                     self.force_refresh_interval,
                 )
-            except Exception as Argument : 
-                await self.hass.async_add_executor_job(
-                self.vehicle_manager.update_all_vehicles_with_cached_state
-                )
-                _LOGGER.exception("Force update failed, falling back to cached.")
+            except Exception as err:                 
+                try:
+                    await self.hass.async_add_executor_job(
+                    self.vehicle_manager.update_all_vehicles_with_cached_state
+                    )
+                    _LOGGER.exception("Force update failed, falling back to cached: {err}")
+                except Exception as err_nested:
+                    raise UpdateFailed(f"Error communicating with API: {err_nested}")
+
                 
                 
         else:
