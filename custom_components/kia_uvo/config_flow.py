2f0b3eca5efaad643772488bee3b37638e3d0e0f
cdnninja <cdnninja@users.noreply.github.com>
2023-01-07 02:28:03 +0000

    fix: black action

--- custom_components/kia_uvo/config_flow.py
+++ custom_components/kia_uvo/config_flow.py
@@ -163,15 +163,17 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            if self._reauth_entry is False: 
+            if self._reauth_entry is False:
                 title = f"{BRANDS[user_input[CONF_BRAND]]} {REGIONS[user_input[CONF_REGION]]} {user_input[CONF_USERNAME]}"
                 await self.async_set_unique_id(
                     hashlib.sha256(title.encode("utf-8")).hexdigest()
                 )
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(title=title, data=user_input)
-            else: 
-                self.hass.config_entries.async_update_entry(self.reauth_entry, data=user_input)
+            else:
+                self.hass.config_entries.async_update_entry(
+                    self.reauth_entry, data=user_input
+                )
                 await self.hass.config_entries.async_reload(self.reauth_entry.entry_id)
                 return self.async_abort(reason="reauth_successful")
 
